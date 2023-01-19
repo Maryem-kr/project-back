e4b23a2abb9409693f5751dc18bee4c09a5bf645
rim-krouma <rimkrouma90@gmail.com>
2023-01-19 21:00:37 +0100

    first commit

---
+++ src/main/java/com/replanner/ReplannerApplication.java
@@ -0,0 +1,13 @@
+package com.replanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/replanner/ReplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.replanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
